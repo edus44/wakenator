ac5cc5bcf0b59c3e52dbe5b0b780c7840a738e73
Eduardo Hidalgo <Eduardo Hidalgo>
2016-02-07 16:07:11 +0100

    initial

---
+++ main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const debug = require('debug')('main');
+
+const electron = require('electron');
+const app = electron.app;
+
+const menu = require('./lib/menu');
+
+
+app.on('ready', ()=> {   
+    debug('READY');
+    // setTimeout(app.quit,1000);
+
+    menu.initialize();
+});
+
+
