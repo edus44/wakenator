f80f84e6d482f969679a378911b392f20547f539
edus44 <eduhhg@gmail.com>
2017-09-23 23:12:43 +0200

    transparent window

--- window/src/components/App.vue
+++ window/src/components/App.vue
@@ -17,4 +17,25 @@ export default {
 
 <style lang="scss">
 @import '~bulma';
+body,html{
+    background-color: transparent;
+    width:100%;
+    height: 100%;
+    min-height: 100%;
+    margin:0;
+    padding: 0;
+    overflow: hidden;
+}
+body{
+    display: flex;
+    // background-color: #fc0;
+    padding: 6px;
+}
+div{
+    flex:1;
+    background-color: #ccc;
+    border-radius: 6px;
+    text-align: center;
+    box-shadow: 0 0 6px 2px rgba(0,0,0,.2)
+}
 </style>
