d10a07d3e00f049b02e5a546492138e347b636e2
edus44 <eduhhg@gmail.com>
2017-09-24 13:55:55 +0200

    wip

--- desktop/lib/WkTray.js
+++ desktop/lib/WkTray.js
@@ -2,28 +2,33 @@
 
 const { Tray, Menu } = require('electron')
 const path = require('path')
-const assetsDirectory = path.resolve(__dirname, '../res')
 const {EventEmitter} = require('events')
 const debug = require('debug')('wk:tray')
+
+function getAsset(file){
+    return path.resolve(__dirname, '../res',file)
+}
 
 module.exports = class WkTray extends EventEmitter{
     constructor(){
         super()
         debug('init')
 
-        let icon  = path.join(assetsDirectory, 'icon-white.png')
-
-        this.tray = new Tray(icon)
+        this.tray = new Tray(getAsset('icon-white.png'))
         this.tray.on('click', this.click.bind(this))
 
+        if (process.platform=='darwin'){
+            this.tray.setImage(getAsset('iconTemplate.png'))
+            this.tray.setPressedImage(getAsset('iconHover.png'))
+        }
 
-        const contextMenu = Menu.buildFromTemplate([
-            {label: 'Item1', type: 'radio'},
-            {label: 'Item2', type: 'radio'},
-            {label: 'Item3', type: 'radio', checked: true},
-            {label: 'Item4', type: 'radio'}
-        ])
-        this.tray.setContextMenu(contextMenu)
+        // const contextMenu = Menu.buildFromTemplate([
+        //     {label: 'Item1', type: 'radio'},
+        //     {label: 'Item2', type: 'radio'},
+        //     {label: 'Item3', type: 'radio', checked: true},
+        //     {label: 'Item4', type: 'radio'}
+        // ])
+        // this.tray.setContextMenu(contextMenu)
 
         // this.tray.on('right-click', this.click.bind(this))
         this.tray.setToolTip('Wakenator')
