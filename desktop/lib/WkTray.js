39b4975f99d5349e0e59cfdda1c359cdb74765bd
edus44 <eduhhg@gmail.com>
2017-09-24 13:52:47 +0200

    wip

--- desktop/lib/WkTray.js
+++ desktop/lib/WkTray.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { Tray } = require('electron')
+const { Tray, Menu } = require('electron')
 const path = require('path')
 const assetsDirectory = path.resolve(__dirname, '../res')
 const {EventEmitter} = require('events')
@@ -15,7 +15,17 @@ module.exports = class WkTray extends EventEmitter{
 
         this.tray = new Tray(icon)
         this.tray.on('click', this.click.bind(this))
-        this.tray.on('right-click', this.click.bind(this))
+
+
+        const contextMenu = Menu.buildFromTemplate([
+            {label: 'Item1', type: 'radio'},
+            {label: 'Item2', type: 'radio'},
+            {label: 'Item3', type: 'radio', checked: true},
+            {label: 'Item4', type: 'radio'}
+        ])
+        this.tray.setContextMenu(contextMenu)
+
+        // this.tray.on('right-click', this.click.bind(this))
         this.tray.setToolTip('Wakenator')
     }
 
