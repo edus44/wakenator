0e072985d005410ccf09b42554f8cb5d490c05c3
edus44 <eduhhg@gmail.com>
2018-06-10 11:12:07 +0200

    global essc

--- app/src/components/App.vue
+++ app/src/components/App.vue
@@ -2,17 +2,26 @@
   <div>
     <Tray/>
     <Wake/>
+
+    <GlobalEvents
+      @keyup.escape="minimize"
+    />
   </div>
 </template>
 
 <script>
 import Tray from '@/components/views/Tray'
 import Wake from '@/components/views/Wake'
+import GlobalEvents from 'vue-global-events'
+import { minimize } from '@/lib/win'
 
 export default {
-  components: { Tray, Wake },
+  components: { Tray, Wake, GlobalEvents },
   created() {
     this.$store.dispatch('user/init')
+  },
+  methods: {
+    minimize,
   },
 }
 </script>
