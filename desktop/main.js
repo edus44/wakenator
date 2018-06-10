74055da50390ba4facdca066e80632bd482a1c2f
edus44 <eduhhg@gmail.com>
2018-06-11 00:42:45 +0200

    travis

--- desktop/main.js
+++ desktop/main.js
@@ -4,7 +4,7 @@ const tray = require('./lib/tray')
 const { app } = require('electron')
 const debug = require('debug')('wk:main')
 const AutoLaunch = require('auto-launch')
-
+const { autoUpdater } = require('electron-updater')
 debug('init')
 
 // Event logs
@@ -19,3 +19,7 @@ app.on('window-all-closed', () => {
 tray.init()
 
 new AutoLaunch({ name: 'Wakenator' }).enable()
+
+autoUpdater.checkForUpdatesAndNotify()
+autoUpdater.logger = require('electron-log')
+autoUpdater.logger.transports.file.level = 'debug'
